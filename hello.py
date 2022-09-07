7cb5f17183de8d999bfb0385e949e34e660fb9f0
Serg Kopejka <sergk1972@gmail.com>
2022-09-07 16:11:18 +0300

    divine feature added

--- hello.py
+++ hello.py
@@ -4,6 +4,12 @@ def summary(x, y):
 def minus(*args):
     return x - y
 
+def devine(x, y):
+    if y != 0:
+        return x/y
+    else:
+        print("0 is forbidden!")
+
 print(x:="Hello World!")
 print(y:="Work hard!")
 print(f"{y} and {x}")
